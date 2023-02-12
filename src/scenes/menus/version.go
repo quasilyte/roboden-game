f0d8bd33e1eff985f01490959c17a39f8eb87a4a
Iskander Sharipov <quasilyte@gmail.com>
2023-02-13 03:17:19 +0400

    describe changes made so far in the version file

--- src/scenes/menus/version.go
+++ src/scenes/menus/version.go
@@ -3,4 +3,13 @@ package menus
 // Version 2
 // - Added min movement radius to avoid accidental clicks
 // - Fixed mismatching sprite size
-const buildNumber int = 2
+//
+// Version 3 (post jam)
+// - Improved performance
+// - New font
+// - Updated drone cloning algorithm
+// - Fixed a cloning price bug in colony planner
+// - Fixed colony movement overjump
+// - Fixed some typos
+// - Fixed some text rendering issues (it was too blurry at times)
+const buildNumber int = 3

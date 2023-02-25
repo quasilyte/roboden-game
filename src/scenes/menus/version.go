e141c643a0e59273e5fd06326c43c0e169368408
Iskander Sharipov <quasilyte@gmail.com>
2023-02-26 01:04:35 +0400

    update version file

--- src/scenes/menus/version.go
+++ src/scenes/menus/version.go
@@ -28,6 +28,7 @@ package menus
 //   - Added mountains as a higher height wall types
 //   - Crawler creeps (spawned by a boss)
 //   - Added mortar drone
+//   - Added anti-air missile drone
 //
 // * Visual improvements:
 //   - Added a flash effect when a unit (friendly or not) takes damage

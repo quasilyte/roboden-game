d4ac84e06492e5e1f36a130b9f285c276efc765e
Iskander Sharipov <quasilyte@gmail.com>
2023-03-01 03:48:13 +0400

    never generate less than 2 red oil sources
    Even on a very small map with poor resources.

--- src/scenes/staging/level_generator.go
+++ src/scenes/staging/level_generator.go
@@ -214,7 +214,7 @@ func (g *levelGenerator) placeResources(resMultiplier float64) {
 	numGold := int(float64(rand.IntRange(20, 28)) * multiplier)
 	numCrystals := int(float64(rand.IntRange(14, 20)) * multiplier)
 	numOil := int(float64(rand.IntRange(4, 6)) * multiplier)
-	numRedOil := int(float64(rand.IntRange(2, 3)) * multiplier)
+	numRedOil := gmath.ClampMin(int(float64(rand.IntRange(2, 3))*multiplier), 2)
 
 	g.sectorSlider.TrySetValue(rand.IntRange(0, len(g.sectors)-1))
 

f78290ccaa93b223b6d7a4a120e1dba3576f41d3
Iskander Sharipov <quasilyte@gmail.com>
2023-03-01 00:13:24 +0400

    improve crawlers behavior

--- src/scenes/staging/creep_coordinator.go
+++ src/scenes/staging/creep_coordinator.go
@@ -1,8 +1,6 @@
 package staging
 
 import (
-	"fmt"
-
 	"github.com/quasilyte/gmath"
 )
 
@@ -22,33 +20,37 @@ type creepCoordinator struct {
 
 	scoutingDelay float64
 	attackDelay   float64
+	scatterDelay  float64
 }
 
 func newCreepCoordinator(world *worldState) *creepCoordinator {
 	return &creepCoordinator{
-		world:       world,
-		crawlers:    make([]*creepNode, 0, 16),
-		groupSlice:  make([]*creepNode, 0, 4),
-		attackDelay: world.rand.FloatRange(10, 30),
+		world:        world,
+		crawlers:     make([]*creepNode, 0, 16),
+		groupSlice:   make([]*creepNode, 0, 4),
+		attackDelay:  world.rand.FloatRange(10, 30),
+		scatterDelay: world.rand.FloatRange(2*60, 3*60),
 	}
 }
 
 func (c *creepCoordinator) Update(delta float64) {
-	c.attackDelay = gmath.ClampMin(c.attackDelay-delta, 0)
-	c.scoutingDelay = gmath.ClampMin(c.scoutingDelay-delta, 0)
-
 	if len(c.crawlers) == 0 {
 		// No units to coordinate, try later.
-		c.attackDelay = c.world.rand.FloatRange(6.0, 10.0)
-		c.scoutingDelay += 2 + delta*8
 		return
 	}
+
+	c.attackDelay = gmath.ClampMin(c.attackDelay-delta, 0)
+	c.scoutingDelay = gmath.ClampMin(c.scoutingDelay-delta, 0)
+	c.scatterDelay = gmath.ClampMin(c.scatterDelay-delta, 0)
 
 	if c.attackDelay == 0 {
 		c.tryLaunchingAttack()
 	}
 	if c.scoutingDelay == 0 {
 		c.sendScout()
+	}
+	if c.scatterDelay == 0 {
+		c.tryLaunchingScatter()
 	}
 }
 
@@ -64,8 +66,7 @@ func (c *creepCoordinator) sendScout() {
 		return
 	}
 
-	fmt.Println("launch creep scout")
-	c.scoutingDelay = c.world.rand.FloatRange(10.0, 30.0)
+	c.scoutingDelay = c.world.rand.FloatRange(30.0, 50.0)
 
 	scoutingDist := 256 * c.world.rand.FloatRange(1, 2)
 	scoutingDest := gmath.RadToVec(c.world.rand.Rad()).Mulf(scoutingDist).Add(scout.pos)
@@ -73,6 +74,32 @@ func (c *creepCoordinator) sendScout() {
 	scout.waypoint = c.world.pathgrid.AlignPos(scout.pos)
 	p := c.world.BuildPath(scout.waypoint, scoutingDest)
 	scout.path = p.Steps
+}
+
+func (c *creepCoordinator) tryLaunchingScatter() {
+	leader := gmath.RandElem(c.world.rand, c.crawlers)
+	if leader.specialModifier != crawlerIdle {
+		c.scatterDelay = c.world.rand.FloatRange(4, 10)
+		return
+	}
+
+	group := c.collectGroup(leader)
+	if len(group) < 2 {
+		c.scatterDelay = c.world.rand.FloatRange(8, 14)
+		return
+	}
+
+	c.scatterDelay = c.world.rand.FloatRange(70, 90)
+
+	for _, creep := range group {
+		dist := c.world.rand.FloatRange(96, 192)
+		targetPos := gmath.RadToVec(c.world.rand.Rad()).Mulf(dist).Add(creep.pos)
+
+		creep.specialModifier = crawlerMove
+		p := c.world.BuildPath(creep.pos, targetPos)
+		creep.path = p.Steps
+		creep.waypoint = c.world.pathgrid.AlignPos(creep.pos)
+	}
 }
 
 func (c *creepCoordinator) tryLaunchingAttack() {
@@ -85,28 +112,11 @@ func (c *creepCoordinator) tryLaunchingAttack() {
 	}
 
 	const (
-		maxUnitRange      float64 = 196
-		maxUnitRangeSqr   float64 = maxUnitRange * maxUnitRange
 		maxAttackRange    float64 = 1024.0
 		maxAttackRangeSqr float64 = maxAttackRange * maxAttackRange
 	)
 
-	// Try to build a group of at least 2 units.
-	maxGroupSize := c.world.rand.IntRange(2, cap(c.groupSlice))
-	group := c.groupSlice[:0]
-	group = append(group, leader)
-	for _, creep := range c.crawlers {
-		if len(group) >= maxGroupSize {
-			break
-		}
-		if creep == leader || creep.specialModifier != crawlerIdle {
-			continue
-		}
-		if creep.pos.DistanceSquaredTo(leader.pos) > maxUnitRangeSqr {
-			continue
-		}
-		group = append(group, creep)
-	}
+	group := c.collectGroup(leader)
 
 	attackRangeSqr := maxAttackRangeSqr * c.world.rand.FloatRange(0.8, 1.2)
 
@@ -129,8 +139,7 @@ func (c *creepCoordinator) tryLaunchingAttack() {
 	// Launch the attack.
 
 	// The next action will be much later.
-	fmt.Println("launch creep attack")
-	c.attackDelay = c.world.rand.FloatRange(20.0, 55.0)
+	c.attackDelay = c.world.rand.FloatRange(30.0, 70.0)
 
 	for _, creep := range group {
 		dist := c.world.rand.FloatRange(creep.stats.weapon.AttackRange*0.5, creep.stats.weapon.AttackRange*0.8)
@@ -141,4 +150,30 @@ func (c *creepCoordinator) tryLaunchingAttack() {
 		creep.path = p.Steps
 		creep.waypoint = c.world.pathgrid.AlignPos(creep.pos)
 	}
+}
+
+func (c *creepCoordinator) collectGroup(leader *creepNode) []*creepNode {
+	const (
+		maxUnitRange    float64 = 196
+		maxUnitRangeSqr float64 = maxUnitRange * maxUnitRange
+	)
+
+	// Try to build a group of at least 2 units.
+	maxGroupSize := c.world.rand.IntRange(2, cap(c.groupSlice))
+	group := c.groupSlice[:0]
+	group = append(group, leader)
+	for _, creep := range c.crawlers {
+		if len(group) >= maxGroupSize {
+			break
+		}
+		if creep == leader || creep.specialModifier != crawlerIdle {
+			continue
+		}
+		if creep.pos.DistanceSquaredTo(leader.pos) > maxUnitRangeSqr {
+			continue
+		}
+		group = append(group, creep)
+	}
+
+	return group
 }

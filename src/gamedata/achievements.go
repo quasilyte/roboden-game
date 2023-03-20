beb6fa73e95a1e99cea54c44a9e73228bd85bee2
Iskander Sharipov <quasilyte@gmail.com>
2023-03-20 18:16:36 +0400

    add achievements stub

---
+++ src/gamedata/achievements.go
@@ -0,0 +1,65 @@
+package gamedata
+
+type Achievement struct {
+	Name string
+	Mode string
+}
+
+const (
+	ModeClassic = "classic"
+)
+
+var AchievementList = []*Achievement{
+	{
+		Name: "impossible",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "speedrunning",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "t3engineer",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "t3less",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "cheapbuild10",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "hightension",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "solobase",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "uiless",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "powerof3",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "victorydrag",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "tinyradius",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "t1army",
+		Mode: ModeClassic,
+	},
+	{
+		Name: "groundwin",
+		Mode: ModeClassic,
+	},
+}

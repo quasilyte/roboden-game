33381041791a510c80c9df6a2958aabe0cbd93fd
Iskander Sharipov <quasilyte@gmail.com>
2023-03-24 21:46:27 +0400

    show how many tutorial missions are completed on the progress screen

--- src/scenes/menus/profile_progressmenu.go
+++ src/scenes/menus/profile_progressmenu.go
@@ -74,6 +74,7 @@ func (c *ProfileProgressMenuController) initUI() {
 		fmt.Sprintf("%s: %d/%d", d.Get("menu.profile.progress.drones_unlocked"), len(stats.DronesUnlocked), numDrones),
 		fmt.Sprintf("%s: %d/%d", d.Get("menu.profile.progress.t3drones_seen"), len(stats.Tier3DronesSeen), len(gamedata.Tier3agentMergeRecipes)),
 		fmt.Sprintf("%s: %d/3", d.Get("menu.profile.progress.modes_unlocked"), modesUnlocked),
+		fmt.Sprintf("%s: %d/%d", d.Get("menu.profile.progress.tutorials_completed"), len(stats.TutorialsCompleted), len(gamedata.Tutorials)),
 	}
 	rowContainer.AddChild(eui.NewCenteredLabel(uiResources, strings.Join(lines, "\n"), smallFont))
 

73e69215b509943627927fc55c8c9d5b4ef99411
Iskander Sharipov <quasilyte@gmail.com>
2023-04-13 11:27:39 +0400

    add gradient to the start/end of the slide_x shader

--- src/assets/_data/shader/slide_x.go
+++ src/assets/_data/shader/slide_x.go
@@ -11,6 +11,16 @@ func Fragment(_ vec4, texCoord vec2, _ vec4) vec4 {
 	width := pixSize.x * srcRegion.x
 	actualTexPos := texCoord - originTexPos
 	actualPixPos := actualTexPos * pixSize
-	actualPixPos.x = mod(actualPixPos.x-(100*Time), width)
-	return imageSrc0At(actualPixPos/pixSize + originTexPos)
+	slidePixPos := vec2(mod(actualPixPos.x-(100*Time), width), actualPixPos.y)
+	c := imageSrc0At(slidePixPos/pixSize + originTexPos)
+	if actualPixPos.x <= 10.0 {
+		// width=110 x=2 a=0.2
+		// width=110 x=8 a=0.8
+		c *= actualPixPos.x * 0.1
+	} else if actualPixPos.x >= (width - 10.0) {
+		// width=110 x=102 delta=8 a=0.8
+		// width=110 x=108 delta=2 a=0.2
+		c *= (width - actualPixPos.x) * 0.1
+	}
+	return c
 }

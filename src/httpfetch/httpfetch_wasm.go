c1cf135454f0fdd206ef769ed5e326cd2612bd80
Iskander Sharipov <quasilyte@gmail.com>
2023-04-29 15:12:22 +0400

    make http post work in wasm too

--- src/httpfetch/httpfetch_wasm.go
+++ src/httpfetch/httpfetch_wasm.go
@@ -7,33 +7,63 @@ import (
 	"syscall/js"
 )
 
+type Response struct {
+	Data []byte
+	Code int
+}
+
+func PostJSON(targetURL string, jsonBytes []byte) (Response, error) {
+	res := doFetch(targetURL, map[string]any{
+		"method": "POST",
+		"headers": map[string]any{
+			"Accept":       "application/json",
+			"Content-Type": "application/json",
+		},
+		"body": string(jsonBytes),
+	})
+	return Response{Data: res.data, Code: res.status}, res.err
+}
+
 func GetBytes(targetURL string) ([]byte, error) {
-	type result struct {
-		data []byte
-		err  error
+	res := doFetch(targetURL, nil)
+	return res.data, res.err
+}
+
+type fetchResult struct {
+	data   []byte
+	status int
+	err    error
+}
+
+func doFetch(targetURL string, params map[string]any) fetchResult {
+	ch := make(chan fetchResult)
+
+	var fetch js.Value
+	if len(params) == 0 {
+		fetch = js.Global().Call("fetch", targetURL)
+	} else {
+		fetch = js.Global().Call("fetch", targetURL, params)
 	}
 
-	ch := make(chan result)
-
-	fetch := js.Global().Call("fetch", targetURL)
 	fetch.Call("then", js.FuncOf(func(this js.Value, args []js.Value) any {
+		status := args[0].Get("status").Int()
 		args[0].Call("arrayBuffer").Call("then", js.FuncOf(func(this js.Value, args []js.Value) any {
 			size := args[0].Get("byteLength").Int()
 			data := make([]byte, size)
 			u8array := js.Global().Get("Uint8Array").New(args[0])
 			numBytes := js.CopyBytesToGo(data, u8array)
 			if numBytes != size {
-				ch <- result{err: errors.New("incomplete bytes copy")}
+				ch <- fetchResult{status: status, err: errors.New("incomplete bytes copy")}
 			}
-			ch <- result{data: data}
+			ch <- fetchResult{status: status, data: data}
 			return nil
 		}))
 		return nil
 	})).Call("catch", js.FuncOf(func(this js.Value, args []js.Value) any {
-		ch <- result{err: errors.New(args[0].Get("message").String())}
+		ch <- fetchResult{err: errors.New(args[0].Get("message").String())}
 		return nil
 	}))
 
 	res := <-ch
-	return res.data, res.err
+	return res
 }

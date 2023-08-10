ca3b23f02970ec57323fb2c8abab31f6fc8a91cf
Iskander Sharipov <quasilyte@gmail.com>
2023-08-11 01:08:46 +0400

    fix seed validation

--- src/gamedata/validate.go
+++ src/gamedata/validate.go
@@ -40,7 +40,7 @@ func IsSendableReplay(r serverapi.GameReplay) bool {
 	if !IsRunnableReplay(r) {
 		return false
 	}
-	if GetSeedKind(r.Config.Seed, r.Config.RawGameMode) == SeedNormal {
+	if GetSeedKind(r.Config.Seed, r.Config.RawGameMode) != SeedNormal {
 		return false
 	}
 	if r.Results.Score <= 0 {

93b86ea9249fd0f02cdd7c8c325ba9daf8503333
Limarev Denis <denislimarev@gmail.com>
2023-08-14 22:33:16 +0500

    on click back from stagingOptionsMenu exit to mainMenu

--- src/scenes/menus/staging_optionsmenu.go
+++ src/scenes/menus/staging_optionsmenu.go
@@ -88,7 +88,7 @@ func (c *StagingOptionsMenuController) initUI() {
 	rowContainer.AddChild(eui.NewSeparator(widget.RowLayoutData{Stretch: true}))
 
 	rowContainer.AddChild(eui.NewButton(uiResources, c.scene, d.Get("menu.back"), func() {
-		c.back()
+		c.scene.Context().ChangeScene(NewMainMenuController(c.state))
 	}))
 
 	uiObject := eui.NewSceneObject(root)
